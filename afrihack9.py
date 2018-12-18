2048e25dd4c1f6c2bc493e1c35bc19d27f28cabf
Wybe Turing <karllemfon@gmail.com>
2018-12-18 22:14:04 +0000

    I experimented with indexing strings using the double standard.

--- afrihack9.py
+++ afrihack9.py
@@ -2,9 +2,11 @@ def add_length(str_):
     string = str_.split(" ")
     wlen = []
     ans = []
+    print(string[1][1:4])
     for i in range(0,len(string)):
         l = len(string[i])
         wlen.append(l)
     for i in range(0, len(string)):
         ans.append(string[i]+ " " + str(wlen[i]))
     return ans
+add_length("gajhkalk hkdjlakd bdhakdaj balka")
