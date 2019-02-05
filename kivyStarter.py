c95758875df84eb147e92c47ffc67b872328d18c
WybeTuring <Karllemfon@gmail.com>
2019-02-05 23:10:44 +0000

    extending the application to try out the multi-grid system

--- kivyStarter.py
+++ kivyStarter.py
@@ -2,11 +2,26 @@ import kivy
 kivy.require('1.0.6')
 from kivy.app import App
 from kivy.uix.label import Label
+from kivy.uix.gridlayout import gridlayout
+from kivy.uix.textinput import TextInput
+
+
+
+class LoginScreen(GridLayout):
+    def __init(self, **kwargs):
+        super(LoginScreen, self).__init__(**kwargs)
+        self.cols = 2
+        self.add_widget(Label(text = "User Name"))
+        self.username = TextInput(multiline = False)
+        self.add_widget(self.username)
+        self.add_widget(Label(text = "password"))
+        self.password = TextInput(password=True, multiline=False)
+        self.add_widget(self.password)
 
 class MyApp(App):
 
     def build(self):
-        return Label(text = "This is my first kivy application")
-
+        return LoginScreen()
+        
 if __name__ == "__main__":
     MyApp().run()
