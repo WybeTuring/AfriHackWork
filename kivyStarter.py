0ab94c5cf15025dddb95d64e4f85fbcfdeb6226c
WybeTuring <Karllemfon@gmail.com>
2019-02-06 00:00:16 +0000

    First work with Tkinter

--- kivyStarter.py
+++ kivyStarter.py
@@ -2,10 +2,8 @@ import kivy
 kivy.require('1.0.6')
 from kivy.app import App
 from kivy.uix.label import Label
-from kivy.uix.gridlayout import gridlayout
+from kivy.uix.gridlayout import GridLayout
 from kivy.uix.textinput import TextInput
-
-
 
 class LoginScreen(GridLayout):
     def __init(self, **kwargs):
@@ -18,10 +16,10 @@ class LoginScreen(GridLayout):
         self.password = TextInput(password=True, multiline=False)
         self.add_widget(self.password)
 
-class MyApp(App):
+class KarlFirst(App):
 
     def build(self):
         return LoginScreen()
-        
+
 if __name__ == "__main__":
-    MyApp().run()
+    KarlFirst().run()
