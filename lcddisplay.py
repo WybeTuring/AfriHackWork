315a5bbae99848e69b08548fdc03b15b16998109
WybeTuring <Karllemfon@gmail.com>
2019-02-14 14:38:00 +0000

    still looking for a way to display the output on thesame line.

---
+++ lcddisplay.py
@@ -0,0 +1,31 @@
+def printer(a):
+    if a == 1:
+        return ("  |\n  |")
+    elif a == 2:
+        return ("__\n__|\n|__")
+    elif a == 3:
+        return ("__\n__|\n__|")
+    elif a == 4:
+        return ("|__|\n   |")
+    elif a == 5:
+        return (" __\n|__\n___|")
+    elif a == 6:
+        return (" __\n|__\n|__|")
+    elif a == 7:
+        return ("__\n  |\n  |")
+    elif a == 8:
+        return (" __\n|__|\n|__|")
+    elif a == 9:
+        return (" __\n|__|\n __|")
+    elif a == 0:
+        return (" __\n|  |\n|__|")
+    else:
+        print("Ensure that you are entering a digit")
+
+print(printer(0))
+num = list(input("Please type in the number: "))
+num = [int(k) for k in num]
+for k in num:
+    print(printer(k))
+
+    
